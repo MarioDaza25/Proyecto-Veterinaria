6377e588074daea49461077dd9324c456d8cab96
Mario Daza <marioivandaza25@gmail.com>
2023-10-11 21:27:40 -0500

    feat(Persistencia): :zap: Creacion Context

---
+++ Persistencia/DbAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistencia;
+
+public class DbAppContext : DbContext
+{
+    public DbAppContext(DbContextOptions<DbAppContext> options) : base(options)
+    {
+    }
+    //Aqui se establecen los DbSet<Entity> Entities { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Rol> Roles { get; set; }
+    protected override void OnModelCreating( ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
