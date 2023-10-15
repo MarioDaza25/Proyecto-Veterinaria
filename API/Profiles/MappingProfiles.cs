2010b2d25d83e0574d5c3b06bfd1a5eda214a312
Mario Daza <marioivandaza25@gmail.com>
2023-10-15 16:28:51 -0500

    feat(Api): :zap: Implementaciones en MappingProfile

--- API/Profiles/MappingProfiles.cs
+++ API/Profiles/MappingProfiles.cs
@@ -1,4 +1,6 @@
+using API.Dtos;
 using AutoMapper;
+using Dominio.Entidades;
 
 namespace API.Profiles;
 
@@ -6,6 +8,20 @@ public class MappingProfiles : Profile
 {
      public MappingProfiles()
      {
-        //CreateMap<Entidad,EntidadDto>().ReverseMap();
+        CreateMap<Cita, CitaDto>().ReverseMap();
+        CreateMap<DetalleMovimiento, DetalleMovDto>().ReverseMap();
+        CreateMap<Especie, EspecieDto>().ReverseMap();
+        CreateMap<FormulaMedica, FormulaDto>().ReverseMap();
+        CreateMap<Laboratorio, LaboratorioDto>().ReverseMap();
+        CreateMap<Mascota, MascotaDto>().ReverseMap();
+        CreateMap<Medicamento, MedicamentoDto>().ReverseMap();
+        CreateMap<Movimiento, MovimientoDto>().ReverseMap();
+        CreateMap<Propietario, PropietarioDto>().ReverseMap();
+        CreateMap<Proveedor, ProveedorDto>().ReverseMap();
+        CreateMap<RazaDto, RazaDto>().ReverseMap();
+        CreateMap<TipoMovimiento, TipoMovDto>().ReverseMap();
+        CreateMap<Veterinario, VeterinarioDto>().ReverseMap();
+
+        CreateMap<Rol, RolDto>().ReverseMap();
      }
 }
