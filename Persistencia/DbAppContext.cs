a00761e5d3bfbad47498f771f4179e507d54abeb
Mario Daza <marioivandaza25@gmail.com>
2023-10-15 03:10:35 -0500

    feat(Persistencia): :zap: Implementaciones DbContext

--- Persistencia/DbAppContext.cs
+++ Persistencia/DbAppContext.cs
@@ -9,9 +9,28 @@ public class DbAppContext : DbContext
     public DbAppContext(DbContextOptions<DbAppContext> options) : base(options)
     {
     }
-    //Aqui se establecen los DbSet<Entity> Entities { get; set; }
-    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Cita> Citas  { get; set; }
+    public DbSet<DetalleMovimiento> DetalleMovimientos { get; set; }
+    public DbSet<Especie> Especies { get; set; }
+    public DbSet<FormulaMedica> FormulasMedicas { get; set; }
+    public DbSet<Laboratorio> Laboratorios { get; set; }
+    public DbSet<Mascota> Mascotas { get; set; }
+    public DbSet<Medicamento> Medicamentos { get; set; }
+    public DbSet<MedicamentoProveedor> MedicamentosProveedores { get; set; }
+    public DbSet<Movimiento> Movimientos { get; set; }
+    public DbSet<Propietario> Propietarios { get; set; }
+    public DbSet<Proveedor> Proveedores { get; set; }
+    public DbSet<Raza> Razas { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
     public DbSet<Rol> Roles { get; set; }
+    public DbSet<TipoMovimiento> TipoMovimientos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<UsuarioRol> UsuarioRoles { get; set; }
+    public DbSet<Veterinario> Veterinarios { get; set; }
+    
+
+    
+
     protected override void OnModelCreating( ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
