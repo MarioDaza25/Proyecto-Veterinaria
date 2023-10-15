1b06995f541aece99340d49978dd86c14de37719
Mario Daza <marioivandaza25@gmail.com>
2023-10-15 16:28:00 -0500

    feat(Aplicacion): :zap: Implementacion UnitOfWork

--- Aplicacion/UnitOfWork/UnitOfWork.cs
+++ Aplicacion/UnitOfWork/UnitOfWork.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Aplicacion.Repository;
 using Dominio.Interfaces;
 using Persistencia;
@@ -12,20 +13,156 @@ public class UnitOfWork : IUnitOfWork, IDisposable
         _context = context;
     }
 
-    UsuarioRepository _usuario;
+    CitaRepository _cita;
+    DetalleMovRepository _detMov;
+    EspecieRepository _especie;
+    FormulaRepository _forMedica;
+    LaboratorioRepository _laboratorio;
+    MascotaRepository _mascota;
+    MedicamentoRepository _medicamento;
+    MovimientoRepository _movimiento;
+    PropietarioRepository _propietario;
+    ProveedorRepository _proveedor;
+    RazaRepository _raza;
     RolRepository _rol;
+    TipoMovRepository _tipoMovimiento;
+    UsuarioRepository _usuario;
+    VeterinarioRepository _veterinario;
 
-    public IUsuario Usuarios
+
+
+    public ICita Citas
     {
         get
         {
-            if (_usuario is not null)
+            if (_cita is not null)
             {
-                return _usuario;
+                return _cita;
             }
-            return _usuario = new UsuarioRepository(_context);
+            return _cita = new CitaRepository(_context);
         }
     }
+
+    public IDetalleMovimiento DetalleMovimientos 
+    {
+        get
+        {
+            if( _detMov is not null)
+            {
+                return _detMov; 
+            }
+            return _detMov = new DetalleMovRepository(_context);
+        }
+    }
+
+    public IEspecie Especies 
+    {
+        get
+        {
+            if( _especie is not null)
+            {
+                return _especie; 
+            }
+            return _especie = new EspecieRepository(_context);
+        }
+    }
+
+    public IFormulaMedica FormulasMedicas 
+    {
+        get
+        {
+            if( _forMedica is not null)
+            {
+                return  _forMedica;
+            }
+            return _forMedica = new FormulaRepository(_context);
+        }
+    }
+
+    public ILaboratorio Laboratorios 
+    {
+        get
+        {
+            if( _laboratorio is not null)
+            {
+                return _laboratorio ;
+            }
+            return _laboratorio = new LaboratorioRepository(_context);
+        }
+    }
+
+    public IMascota Mascotas 
+    {
+        get
+        {
+            if( _mascota is not null)
+            {
+                return _mascota ;
+            }
+            return _mascota = new MascotaRepository(_context);
+        }
+    }
+
+    public IMedicamento Medicamentos 
+    {
+        get
+        {
+            if( _medicamento is not null)
+            {
+                return _medicamento ;
+            }
+            return _medicamento = new MedicamentoRepository(_context);
+        }
+    }
+
+    public IMovimiento Movimientos 
+    {
+        get
+        {
+            if( _movimiento is not null)
+            {
+                return _movimiento ;
+            }
+            return _movimiento = new MovimientoRepository(_context);
+        }
+    }
+
+    public IPropietario Propietarios 
+    {
+        get
+        {
+            if( _propietario is not null)
+            {
+                return _propietario ;
+            }
+            return _propietario = new PropietarioRepository(_context);
+        }
+    }
+
+    public IProveedor Proveedores 
+    {
+        get
+        {
+            if( _proveedor is not null)
+            {
+                return _proveedor ;
+            }
+            return _proveedor = new ProveedorRepository(_context);
+        }
+    }
+
+    public IRaza Razas 
+    {
+        get
+        {
+            if( _raza is not null)
+            {
+                return _raza;
+            }
+            return _raza = new RazaRepository(_context);
+        }
+    }
+
     public IRol Roles
     {
         get
@@ -37,6 +174,41 @@ public class UnitOfWork : IUnitOfWork, IDisposable
             return _rol = new RolRepository(_context);
         }
     }
+    public ITipoMovimiento TipoMovimientos 
+    {
+        get
+        {
+            if( _tipoMovimiento is not null)
+            {
+                return _tipoMovimiento ;
+            }
+            return _tipoMovimiento = new TipoMovRepository(_context);
+        }
+    }
+    
+    public IUsuario Usuarios
+    {
+        get
+        {
+            if (_usuario is not null)
+            {
+                return _usuario;
+            }
+            return _usuario = new UsuarioRepository(_context);
+        }
+    }
+    public IVeterinario Veterinarios 
+    {
+        get
+        {
+            if( _veterinario is not null)
+            {
+                return _veterinario ;
+            }
+            return _veterinario = new VeterinarioRepository(_context);
+        }
+    }
+
     public void Dispose()
     {
         _context.Dispose();
